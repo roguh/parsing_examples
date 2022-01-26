d78103569a70ad07ab8c51f8e690bf7ab1deca5e
Hugo O. Rivera <hugo@roguh.com>
2022-01-26 09:03:39 -0800

    Show full language name after langdetect

--- nlp.py
+++ nlp.py
@@ -6,6 +6,11 @@ import spacy
 import spacy.tokens.doc
 import spacy.tokens.span
 
+# For Language detection
+import pycountry
+from spacy.language import Language
+from spacy_langdetect import LanguageDetector
+
 # Easy to use API for getting things done, not meant for research
 # The _sm datasets are meant for efficiency, there are others optimized
 # for accuracy.
@@ -15,24 +20,23 @@ nlp("The quick brown fox jumps over the lazy dog")
 nlp_es = spacy.load("es_core_news_sm")
 nlp_es("Hasta mañana, Alberto")
 
-# Adding a pipeline
-from spacy.language import Language
-from spacy_langdetect import LanguageDetector
-
 # Add LanguageDetector and assign it a string name
 @Language.factory("language_detector")
 def create_language_detector(nlp, name):
     return LanguageDetector(language_detection_function=None)
 
 
-nlp_multi_high_quality = spacy.load("xx_sent_ud_sm")
+# For recognizing sentence boundaries
+nlp_multi = spacy.load("xx_sent_ud_sm")
 
 # Should be the last pipeline
-nlp_multi_high_quality.add_pipe("language_detector")
+nlp_multi.add_pipe("language_detector")
 
-nlp_multi_high_quality("Hasta mañana, Alberto")
-nlp_multi_high_quality("The quick brown fox jumps over the lazy dog")
-nlp_multi_high_quality("как ваши дела")
+samples = [
+    "Hola, comó estas?",
+    "The quick brown fox jumps over the lazy dog",
+    "как ваши дела",
+]
 
 DEFAULT_WIDTH = 11
 
@@ -49,8 +53,13 @@ def print_langdetection(
         language_info = doc_or_span._.language
 
         lang = language_info["language"]
+        lang_info = pycountry.languages.get(alpha_2=lang)
+        if lang_info is None:
+            lang_name = "Unknown"
+        else:
+            lang_name = lang_info.name
         score = language_info["score"] * 100
-        print(f"Language: {lang}. Certainty: {score:.3f}%")
+        print(f"{lang_name} ({lang}) text. Certainty: {score:.3f}%")
     except AttributeError:
         # No language detection
         pass
